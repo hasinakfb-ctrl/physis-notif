8678d67af2dcdc11edf7c2bbc81354ec14516890
Ancien123 <ancien.ispm@gmail.com>
2026-07-14 14:48:47 +0300

    fix: update capacitor config for webview permissions

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,13 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.physis.notif',
   appName: 'physis-notif',
-  webDir: 'dist'
+  webDir: 'dist',
+  server: {
+    allowNavigation: [
+      'localhost'
+    ],
+    cleartext: true
+  }
 };
 
 export default config;
